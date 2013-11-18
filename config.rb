71d295d92f9262902ba420035b893f52023f993e
malei0311 <leiman0311@gmail.com>
2013-11-18 16:01:03 +0800

    css compress && topbar fixed

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ css_dir = "styles"
 sass_dir = "sass/dist"
 images_dir = "images"
 javascripts_dir = "scripts"
-environment = :development
+environment = :production
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
